e24cfc4a778a2601c515ee735e9b21af218a15ee
Li Lun Chen <qpalzm126@gmail.com>
2023-11-16 14:44:08 +0800

    AC O(n)

---
+++ 162.find-peak-element.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=162 lang=javascript
+ *
+ * [162] Find Peak Element
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+    const length=nums.length
+    if (length<2 || nums[0]>nums[1]) return 0
+    if (nums[length-1]>nums[length-2]) return length-1
+
+    for (let i=1;i<length;i++){
+      if (nums[i] > nums[i+1] && nums[i] > nums[i-1]) return i
+    }
+    
+
+};
+// @lc code=end
+
