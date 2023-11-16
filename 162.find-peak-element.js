59780e8d818688a2514918e1427414b4ff6e8431
Li Lun Chen <qpalzm126@gmail.com>
2023-11-16 14:58:49 +0800

    AC

--- 162.find-peak-element.js
+++ 162.find-peak-element.js
@@ -17,8 +17,15 @@ var findPeakElement = function(nums) {
     for (let i=1;i<length;i++){
       if (nums[i] > nums[i+1] && nums[i] > nums[i-1]) return i
     }
-    
 
 };
+// var findPeakElement = function (nums) {
+//   const length = nums.length
+//   if (length < 2 || nums[0] > nums[1]) return 0
+//   if (nums[length - 1] > nums[length - 2]) return length - 1
+//   for (let i = 0; i < length; i++) {
+//     if (nums[i] > nums[i + 1]) return i
+//   }
+//   return length - 1
+}
 // @lc code=end
-
