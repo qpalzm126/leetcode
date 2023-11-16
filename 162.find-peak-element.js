4f3cdf957698d4a006d2ef333e8110636077b23e
Li Lun Chen <qpalzm126@gmail.com>
2023-11-16 14:59:35 +0800

    anno

--- 162.find-peak-element.js
+++ 162.find-peak-element.js
@@ -9,16 +9,15 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findPeakElement = function(nums) {
-    const length=nums.length
-    if (length<2 || nums[0]>nums[1]) return 0
-    if (nums[length-1]>nums[length-2]) return length-1
+var findPeakElement = function (nums) {
+  const length = nums.length
+  if (length < 2 || nums[0] > nums[1]) return 0
+  if (nums[length - 1] > nums[length - 2]) return length - 1
 
-    for (let i=1;i<length;i++){
-      if (nums[i] > nums[i+1] && nums[i] > nums[i-1]) return i
-    }
-
-};
+  for (let i = 1; i < length; i++) {
+    if (nums[i] > nums[i + 1] && nums[i] > nums[i - 1]) return i
+  }
+}
 // var findPeakElement = function (nums) {
 //   const length = nums.length
 //   if (length < 2 || nums[0] > nums[1]) return 0
@@ -27,5 +26,5 @@ var findPeakElement = function(nums) {
 //     if (nums[i] > nums[i + 1]) return i
 //   }
 //   return length - 1
-}
+// }
 // @lc code=end
