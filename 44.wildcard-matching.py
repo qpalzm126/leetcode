76c5a19c5b80c9c5e78cc015ea8294722524c3ed
Li Lun Chen <qpalzm126@gmail.com>
2023-11-23 17:30:23 +0800

    up

---
+++ 44.wildcard-matching.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=44 lang=python3
+#
+# [44] Wildcard Matching
+#
+
+# @lc code=start
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        if s<1 and p<1:
+            return True
+        if p.find('*') <=0:
+            
+    # def mi(self,s:str)->bool:
+        
+# @lc code=end
+
